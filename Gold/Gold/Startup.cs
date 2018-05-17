658c752d003c38deaf187cacf7959030e11d774c
Ana Selegova <ana.selegova@geeks.ltd.uk>
2018-05-17 13:00:46 +0100

    initial

---
+++ Gold/Gold/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gold.Startup))]
+namespace Gold
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
